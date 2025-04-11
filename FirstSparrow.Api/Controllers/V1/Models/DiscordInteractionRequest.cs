ef048243e4784fb20937235550f3276232d4388d
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-04-12 02:45:51 +0400

    fix discord request problenm

--- FirstSparrow.Api/Controllers/V1/Models/DiscordInteractionRequest.cs
+++ FirstSparrow.Api/Controllers/V1/Models/DiscordInteractionRequest.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
 using FirstSparrow.Api.Controllers.V1.Enums;
 
 namespace FirstSparrow.Api.Controllers.V1.Models;
 
 public class DiscordInteractionRequest
 {
+    [JsonPropertyName("type")]
     public DiscordInteractionType Type { get; set; }
 }
