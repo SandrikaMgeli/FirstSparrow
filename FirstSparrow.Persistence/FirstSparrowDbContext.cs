129012731ab08ec6eaf699eb2251c897eeceead8
SandrikaMgeli <kavlashvilisandro1@gmail.com>
2025-05-15 03:28:29 +0400

    configuration done

--- FirstSparrow.Persistence/FirstSparrowDbContext.cs
+++ FirstSparrow.Persistence/FirstSparrowDbContext.cs
@@ -1,8 +1,15 @@
+using FirstSparrow.Persistence.Repositories.Base;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace FirstSparrow.Persistence;
 
-public class FirstSparrowDbContext : DbContext
+public class FirstSparrowDbContext(
+    IOptions<ConnectionStringProvider> connectionStringOptions) : DbContext
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(connectionStringOptions.Value.ConnectionString);
+        optionsBuilder.UseSnakeCaseNamingConvention();
+    }
 }
